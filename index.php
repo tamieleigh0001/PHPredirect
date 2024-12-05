cb7c6b3140e4c3d73404ad4a3a61921e86f3640e
tamieleigh0001 <156190168+tamieleigh0001@users.noreply.github.com>
2024-12-05 11:23:26 +0100

    index.html

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// This line must be before any output is sent to the browser
-header("Location: https://onlinesecurtycheck.com", true, 301); // Change to the URL you want to redirect to
-exit; // Always call exit after a redirect to prevent further script execution
-?>
